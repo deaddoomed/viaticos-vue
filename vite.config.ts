7a960fca0b37ac417695659cbe9059ff97554c19
dead doomed <106939748+deaddoomed@users.noreply.github.com>
2025-11-19 18:35:12 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'https://evperezf/Duoc-Vina/tree/main/Jorge%20Gomez/PTY4614-006V/Equipo%203/Fase%202/Evidencias%20Proyecto/viatico_vuets/'
+  base: 'https://deaddoomed.github.io/viaticos-vue/'
 })
