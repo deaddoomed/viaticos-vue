3de2c1c1d617c292450917d170c814bda56ab7b5
dead doomed <106939748+deaddoomed@users.noreply.github.com>
2025-11-19 18:42:14 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import router from './router'
 import App from './App.vue'
+import * as style from './style.css'
 
 const app = createApp(App).use(router)
 const pinia = createPinia()
